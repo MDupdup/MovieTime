61ef21d6b4454415c42c287ece3ad381ccca08de
KirianCaumes <kirian.caumes@ynov.com>
2018-12-23 19:30:50 +0100

    even better if you had new files

---
+++ src/pages/movie4Tonight/movie4Tonight.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-movie4Tonight',
+  templateUrl: 'movie4Tonight.html'
+})
+export class Movie4TonightPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
