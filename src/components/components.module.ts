6247fda68ea40c741a38b67254daa987a3ef5ad7
KirianCaumes <kirian.caumes@ynov.com>
2018-12-26 23:24:58 +0100

    delete useless components & FUCKING FIX THIS MF QRSCANNER

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,22 +1,13 @@
 import { NgModule } from '@angular/core';
-import { MovieApiComponent } from './movie-api/movie-api';
-import { QrCodeScannerComponent } from './qr-code-scanner/qr-code-scanner';
-import { SearchMovieComponent } from './search-movie/search-movie';
 import { MovieItemListComponent } from './movie-item-list/movie-item-list';
 import { SeparatorComponent } from './separator/separator';
 @NgModule({
     declarations: [
-        MovieApiComponent,
-        QrCodeScannerComponent,
-        SearchMovieComponent,
         MovieItemListComponent,
         SeparatorComponent
     ],
     imports: [],
     exports: [
-        MovieApiComponent,
-        QrCodeScannerComponent,
-        SearchMovieComponent,
         MovieItemListComponent,
         SeparatorComponent
     ]
---
+++ src/pages/qrscanner/qrscanner.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { MoviesProvider } from '../../providers/movies/movies';
+import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner';
+
+/**
+ * Generated class for the QrscannerPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+    selector: 'page-qrscanner',
+    templateUrl: 'qrscanner.html',
+})
+export class QrscannerPage {
+
+    constructor(public navCtrl: NavController, public navParams: NavParams, public mProvider: MoviesProvider, public qrScanner: QRScanner) {
+    }
+
+    ionViewDidLoad() {
+        this.qrScanner.prepare()
+            .then((status: QRScannerStatus) => {
+                if (status.authorized) {
+                    this.qrScanner.show()
+                    window.document.querySelector('ion-app').classList.add('cameraView');
+
+                    let scanSub = this.qrScanner.scan().subscribe((text: string) => {
+                        this.navCtrl.pop();
+                        this.navCtrl.push("DetailPage")
+                        this.mProvider.setMovieId(parseInt(text))
+
+                        this.qrScanner.hide();
+                        scanSub.unsubscribe();
+                        //window.document.querySelector('ion-app').classList.remove('cameraView');
+                    });
+                } else if (status.denied) {
+                    // camera permission was permanently denied
+                } else {
+                    // permission was denied, but not permanently. You can ask for permission again at a later time.
+                }
+            })
+            .catch((e: any) => console.log('Error is', e));
+    }
+
+    ionViewWillLeave() {
+        window.document.querySelector('ion-app').classList.remove('cameraView');
+    }
+
+}
