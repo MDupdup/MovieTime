b8411ab4baabf1caa5c8ac6da4c6a4ad4d829273
Caumes Kirian <kirian.caumes@ynov.com>
2019-03-09 14:52:09 +0100

    things done

--- src/components/movie-item-list/movie-item-list.ts
+++ src/components/movie-item-list/movie-item-list.ts
@@ -3,6 +3,7 @@ import { MoviesProvider } from '../../providers/movies/movies';
 import { StorageProvider } from '../../providers/storage/storage';
 import { FavoritesPage } from '../../pages/favorites/favorites';
 import { TranslateProvider } from '../../providers/translate/translate';
+import { Movie } from '../../models/Movie';
 
 /**
  * Generated class for the MovieItemListComponent component.
@@ -15,9 +16,12 @@ import { TranslateProvider } from '../../providers/translate/translate';
     templateUrl: 'movie-item-list.html'
 })
 export class MovieItemListComponent {
-    @Input() movie: string;
+    @Input() movie: Movie;
     @Input() fav: boolean;
 
-    constructor(public mProvider: MoviesProvider, public sProvider: StorageProvider, public favs: FavoritesPage, public t: TranslateProvider) {}
+    constructor(public mProvider: MoviesProvider, public sProvider: StorageProvider, public favs: FavoritesPage, public t: TranslateProvider) {
+        this.movie = new Movie(0, "Undefined", null, null, null, 0)
+        this.fav = false
+    }
 
 }
--- src/pages/favorites/favorites.ts
+++ src/pages/favorites/favorites.ts
@@ -24,11 +24,7 @@ export class FavoritesPage {
     public deleteMovieFromList(movie: Movie) {
         const alert = this.alert.create({
             title: this.t.__('Are you sure about that?'),
-<<<<<<< HEAD
             subTitle: this.t.__('Do you really want to delete the movie ')+'<span style="font-weight: bold;">' + movie.title + '</span> '+this.t.__(' of your favorites?'),
-=======
-            subTitle: this.t.__('Do you really want to delete the movie ') + '<span style="font-weight: bold;">' + movie.getTitle() + '</span> ' + this.t.__(' of your favorites?'),
->>>>>>> 2648f27962b1ad6e1f394bcb5061d7018f27d083
             buttons: [
                 {
                     text: this.t.__('No'),
